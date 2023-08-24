9d54cc7f12d2876b1d861124e824777d462ced2c
sirciv <spencerallan8@gmail.com>
2023-08-23 23:12:23 -0500

    add easter egg

--- js/lights-off.js
+++
@@ -1,9 +0,0 @@
-//Lights off!
-
-let lightButton = document.getElementById('light_button');
-
-
-lightButton.addEventListener('click', function() {
-    document.body.style.backgroundColor = "black";
-    console.log("hello!");
-});
